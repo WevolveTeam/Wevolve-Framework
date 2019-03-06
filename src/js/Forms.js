ffcc9a7436bb01f2ea070cfcd8fbcd92aae19998
Hickacou <leflon.paul@gmail.com>
2019-03-07 00:26:19 +0100

    First designs of Inputs

--- src/js/Forms.js
+++ src/js/Forms.js
@@ -1,8 +1,10 @@
-function replaceInputHTML(type,id,placeholder,required,email) {
+function replaceInputHTML(type,id,placeholder,required) {
+    // Placeholder : Not a space but a blank character, U+2800 "⠀"
     return `
-    <div id=${id} class='inputContainer'>
-        <input type=${type} class=${email ? "email": ""} required=${required} />
+    <div id='${id}' class='inputContainer'>
+        <input type='${type}' ${required || ""} placeholder="⠀" />
         <div class='animatedPlaceholder'>${placeholder}</div>
+        <div class='animatedBottomLine'></div>
     </div>
     `;
 }
@@ -11,5 +13,8 @@ function replaceInputHTML(type,id,placeholder,required,email) {
 var allInputs = document.querySelectorAll("input[type='text'],input[type='password']");
 
 for (let i of allInputs) {
-
+    let e = document.createElement("div");
+    e.innerHTML = replaceInputHTML(i.getAttribute("type"),i.id,i.getAttribute("placeholder"),i.getAttribute("required"));
+    i.insertAdjacentElement("afterend",e);
+    i.remove();
 }
