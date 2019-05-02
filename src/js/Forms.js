60153ffbffe784c4863a7d9caaeec31960f872d3
Hickacou <leflon.paul@gmail.com>
2019-05-02 16:48:23 +0200

    Added style for input : -Checkbox and <table> element

--- src/js/Forms.js
+++ src/js/Forms.js
@@ -1,10 +1,20 @@
-function replaceInputs(input = new HTMLInputElement) {
+
+var allInputs = document.querySelectorAll("input");
+console.log(allInputs);
+for (var input of allInputs) {
+    replaceInputs(input);
+}
+
+
+function replaceInputs(input) {
     var type = input.type,
         id = input.id || `${input.name}__${input.value}`,
         name = input.name
     ;
     var container = document.createElement("div");
     container.id = id;
+    console.log(input);
+    if (input.getAttribute("nostyle")) return;
 
     if (type == "text" || type == "password") {
         container.className = "textInputContainer";
@@ -18,20 +28,20 @@ function replaceInputs(input = new HTMLInputElement) {
         container.innerHTML = `
         <input type="radio" class='styledRadioButton' name='${name}' value='${input.value}' id='radio__${id}' />
         <label for='radio__${id}' class='displayRadioButton'></label>
-        <label for='radio__${id}' class='styledRadioButtonLabel'>${input.getAttribute("label")}</label>
+        <label for='radio__${id}' class='styledRadioButtonLabel'>${input.getAttribute("label") || ""}</label>
         `;
-    }
+    } else if (type == "checkbox") {
+        container.className = "checkboxInputContainer";
+        container.innerHTML = `
+        <input type="checkbox" class='styledCheckbox' name='${name}' value='${input.value}' id='checkbox__${id}' />
+        <label for='checkbox__${id}' class='displayCheckbox'></label>
+        <label for='checkbox__${id}' class='styledCheckboxLabel'>${input.getAttribute("label") || ""}</label>
+        `;
+    } else {return;}
 
 
     input.insertAdjacentElement("afterend",container);
     input.remove();
+    return;
 
-
-}
-
-
-var allInputs = document.getElementsByTagName("input");
-
-for (var input of allInputs) {
-    replaceInputs(input);
 }
